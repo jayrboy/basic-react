7f6367e07c087f6b8bb319582c9486d8b70cddf8
jayrboy <j.jayox_@hotmail.com>
2023-12-29 15:49:57 +0700

    Add logo.png and Update Header.js index.html index.css
    Add logo.png and Update Header.js index.html index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import Logo from "../images/logo.png";
+
 export default function Header() {
   return (
-    <>
+    <nav>
+      <img src={Logo} alt="logo" />
+      <a href=" ">หน้าแรก</a>
       <h1>JayrBoy</h1>
-    </>
+    </nav>
   );
 }
