1ea3e175deb209c9351731bc972bf091ab668a95
jayrboy <j.jayox_@hotmail.com>
2023-12-29 20:35:05 +0700

    Update onChange AddFor.js
    Update onChange AddFor.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./AddForm.css";
 
 export default function AddForm() {
+  const [name, setName] = useState("");
+
   return (
     <section className="container">
       <form>
         <label>ชื่อนักเรียน</label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <button type="submit" className="btn-add">
           บันทึก
         </button>
